4f3750e8fdfc318ba92c9d67922074aa6dac0607
turantg <ttg4426@gmail.com>
2023-05-09 19:44:06 +0300

    Version1

---
+++ src/main/java/practiceMethod/practiceMethod01.java
@@ -0,0 +1,17 @@
+package practiceMethod;
+
+public class practiceMethod01 {
+
+    public static void main(String[] args) {
+   int sonuc = toplat(2,4);
+        System.out.println(sonuc);
+    }
+
+    public static int toplat(int a , int b){
+
+        return  a + b ;
+
+    }
+
+
+    }
